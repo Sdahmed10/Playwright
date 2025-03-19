d3766da0e550d995cfa4568aef3cd981afb856b2
ahmed <sdiria673@gmail.com>
2025-03-19 08:51:51 +0100

    edit

--- src/test/java/Test2/InteractWithInput.java
+++ src/test/java/Test2/InteractWithInput.java
@@ -25,28 +25,34 @@ public class InteractWithInput {
         //Click Action
         //Saisit le texte "Hello World"
         page.locator("//input[@id='user-message']").type("Hello World");
+
         //Clique sur le bouton pour afficher le message saisi.
         page.locator("//button[@id='showInput']").click();
+
         //Récupère le texte affiché après l'envoi du message.
         String message = page.locator("//p[@id='message']").textContent();
+
         //Affiche le message récupéré dans la console.
         System.out.println("Message: " + message);
 
         //Type vs Fill
         //Navigue vers une autre page
         page.navigate("https://www.lambdatest.com/selenium-playground/generate-file-to-download-demo");
+
         //Tape "Hello World" caractère par caractère
         page.locator("//textarea[@id='textbox']").type("Hello World");
 
 
         //get input values
         page.navigate("https://letcode.in/edit");
+
         // Récupère la valeur pré-remplie d'un champ de texte.
         String inputValue = page.locator("//input[@id='getMe']").inputValue();
         System.out.println(inputValue);
 
         //Récupère l'attribut placeholder d'un champ de texte
         String xpathValue = page.getByPlaceholder("Enter first & last name").getAttribute("placeholder");
+
         //Affiche le placeholder récupéré dans la console.
         System.out.println("placeholder is: " + xpathValue);
 
@@ -54,12 +60,16 @@ public class InteractWithInput {
 
         //how to handle checkbox
         page.navigate("https://www.lambdatest.com/selenium-playground/checkbox-demo");
+
         //Localise la case à cocher 
         Locator isAge = page.locator("//input[@id='isAgeSelected']");
+
         //Vérifie que la case à cocher n'est pas cochée au départ
         assertThat(isAge).not().isChecked();
+
         //Coche la case à cocher.
         isAge.check();
+
         // Vérifie que la case est bien cochée après l'action.
         assertThat(isAge).isChecked();
 
