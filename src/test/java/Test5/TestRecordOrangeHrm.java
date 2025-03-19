e6dee790cad8d32bf5645074efe7517f381fb3e2
ahmed <sdiria673@gmail.com>
2025-03-19 09:48:45 +0100

    add test record

---
+++ src/test/java/Test5/TestRecordOrangeHrm.java
@@ -0,0 +1,28 @@
+package Test5;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+
+
+public class TestRecordOrangeHrm {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Username")).click();
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Username")).press("CapsLock");
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Username")).fill("Admin");
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Username")).press("Tab");
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Password")).fill("admin123");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+            page.getByRole(AriaRole.BANNER).getByRole(AriaRole.IMG, new Locator.GetByRoleOptions().setName("profile picture")).click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Admin")).click();
+            page.getByRole(AriaRole.IMG, new Page.GetByRoleOptions().setName("profile picture")).click();
+            page.getByRole(AriaRole.MENUITEM, new Page.GetByRoleOptions().setName("Logout")).click();
+            page.locator("#app div").filter(new Locator.FilterOptions().setHasText("LoginUsername : AdminPassword")).nth(3).click();
+        }
+    }
+}
+
